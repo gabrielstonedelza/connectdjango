1985825a809edf863a32a32216c004eb9a06461c
Gabriel Nimako Mensah <gabrielstonedelza@gmail.com>
2020-09-15 22:52:37 +0200

    edited settings.py file

--- connectdjango/settings.py
+++ connectdjango/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['157.230.118.219']
+ALLOWED_HOSTS = ['68.183.30.214']
 
 
 # Application definition
