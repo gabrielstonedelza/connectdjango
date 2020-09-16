dda69daa05b7e3923d38c598e5fb49f6092beadb
Gabriel Nimako Mensah <gabrielstonedelza@gmail.com>
2020-09-16 09:26:16 +0200

    solved dtime errors

--- static/blog/js/dtimejs.js
+++ static/blog/js/dtimejs.js
@@ -3,7 +3,7 @@ function monitor_dtime(){
 var mydtimeHolder = document.querySelector("#showdtime")
 // Set the date we're counting down to
 var mydate = new Date()
-var next30min = mydate.getMinutes() + 2
+var next30min = mydate.getMinutes() + 30
 mydate.setMinutes(next30min)
 var mydtime = mydate.getTime()
 
